70b5e585f85d96a2d021e15fe5e849700383a0d1
Emilie3008 <99251374+Emilie3008@users.noreply.github.com>
2024-04-25 13:54:48 +0200

    Update main.py

--- Energy_Grid_Design/main.py
+++ Energy_Grid_Design/main.py
@@ -1,10 +1,12 @@
 
 import matplotlib.pyplot as plt
-from GeneticAlgorithm import GeneticAlgorithm
-from FitnessFunction import plot_vectors_GPT, plot_vectors_XGPT, compute_uncertainty_GPT, compare_integrals, energy_from_energy_grid, cosine_similarity, coarse_energy_grid, energy_cut_from_energy_grid, ECCO33
+from genetic_algorithm import GeneticAlgorithm
+from plot_results import plot_vectors_GPT, plot_vectors_XGPT, compare_integrals
+from data_extraction import coarse_energy_grid, ISOTOPE
+from fitness_utils import energy_from_energy_grid
 import numpy as np
 
-for nGroups in range(7, 13):
+for nGroups in range(7,8):
  
     genetic_algo = GeneticAlgorithm(nGroups)
 
@@ -20,6 +22,7 @@ for nGroups in range(7, 13):
     plt.xscale("log")
     plt.scatter(MY_ENERGY_GRID, np.zeros(nGroups + 1), c = "red")
     plt.xlabel("Energy (MeV)")
-    plt.title("Energy grid ({} groups) found by comparison of the evaluated GPT and xGPT vectors (Pu239, MT = 2, 18, 102)".format(nGroups))
+    plt.title("({}) Energy grid ({} groups) found by comparison of the evaluated GPT "
+              "and xGPT vectors (Pu239, MT = 2, 18, 102)".format(ISOTOPE, nGroups))
 
     plt.show()
