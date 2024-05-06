932a40e4767397936071998215e441b80aa54177
Emilie3008 <99251374+Emilie3008@users.noreply.github.com>
2024-05-06 09:03:22 +0200

    Update fitness_utils.py

--- Energy_Grid_Design/fitness_utils.py
+++ Energy_Grid_Design/fitness_utils.py
@@ -1,17 +1,14 @@
 import numpy as np
-from data_extraction import fine_energy_grid, coarse_energy_grid, extract_data
-from numpy import trapz
+from data_extraction import XGPT_energy_grid, GPT_energy_grid, GPT_vector, xGPT_vector, perts, eigenbasis, singular_matrix 
 
 
-GPT_vector, xGPT_vector, perts, eigenbasis = extract_data()
-
-def down_binning(GPT_sensitivities, GA_energy_grid, coarse_energy_grid):
+def down_binning(GPT_sensitivities, GA_energy_grid, GPT_energy_grid):
     downbinned = {}
     for label, sensitivities in GPT_sensitivities.items():
-        downbinned[label] = evaluate_on_GA(sensitivities, GA_energy_grid, coarse_energy_grid)
+        downbinned[label] = evaluate_on_GA(sensitivities, GA_energy_grid, GPT_energy_grid)
     return downbinned
 
-def evaluate_on_GA(GPT_sensitivities, GA_energy_grid, coarse_energy_grid):
+def evaluate_on_GA(GPT_sensitivities, GA_energy_grid, GPT_energy_grid):
     sensitivities_evaluated = np.zeros((len(GA_energy_grid) + 1,))
     j = 0
     energies = []
@@ -21,8 +18,8 @@ def evaluate_on_GA(GPT_sensitivities, GA_energy_grid, coarse_energy_grid):
         prev_cut = 0 if j == 0 else GA_energy_grid[j - 1]
 
         if i >= prev_cut and i < cut:
-            sensitivities_evaluated[j] += GPT_sensitivities[i]*(coarse_energy_grid[i+1]-coarse_energy_grid[i])
-            energies.append(coarse_energy_grid[i+1]-coarse_energy_grid[i])
+            sensitivities_evaluated[j] += GPT_sensitivities[i]*(GPT_energy_grid[i+1]-GPT_energy_grid[i])
+            energies.append(GPT_energy_grid[i+1] - GPT_energy_grid[i])
 
         else :
             sensitivities_evaluated[j] /= np.sum(energies) 
@@ -31,9 +28,9 @@ def evaluate_on_GA(GPT_sensitivities, GA_energy_grid, coarse_energy_grid):
             energies = []
             cut = len(GPT_sensitivities) if j >= len(GA_energy_grid) else GA_energy_grid[j]
             prev_cut = 0 if j == 0 else GA_energy_grid[j - 1]
-            sensitivities_evaluated[j] += GPT_sensitivities[i]*(coarse_energy_grid[i+1]-coarse_energy_grid[i])
+            sensitivities_evaluated[j] += GPT_sensitivities[i]*(GPT_energy_grid[i+1]-GPT_energy_grid[i])
 
-            energies.append(coarse_energy_grid[i+1]- coarse_energy_grid[i])
+            energies.append(GPT_energy_grid[i+1] - GPT_energy_grid[i])
 
     sensitivities_evaluated[-1] /= np.sum(energies) 
     return sensitivities_evaluated
@@ -51,49 +48,52 @@ def energy_from_energy_grid(energy_discretisation, GA_grid):
     return coarse_energy
 
 # Upbinning to 1500G
-def up_binning(fine_energy_grid, coarse_energy_grid, down_binned_vector):
+def up_binning(XGPT_energy_grid, GPT_energy_grid, down_binned_vector):
     upbinned = {}
     for label, sens in down_binned_vector.items():
-        upbinned[label] = extend(fine_energy_grid, coarse_energy_grid, sens)
+        upbinned[label] = extend(XGPT_energy_grid, GPT_energy_grid, sens)
     return upbinned
 
-def extend(fine_energy_grid, coarse_energy_grid, down_binned_vector):
-    up_binned_vector = np.zeros((len(fine_energy_grid)-1,))
+def extend(XGPT_energy_grid, GPT_energy_grid, down_binned_vector):
+    up_binned_vector = np.zeros( (len(XGPT_energy_grid) - 1, ) )
     j = 1
-    for i in range(1, len(fine_energy_grid)):
-        prev_energy = coarse_energy_grid[j - 1]
-        energy = coarse_energy_grid[j]
+    for i in range(1, len(XGPT_energy_grid)):
+        prev_energy = GPT_energy_grid[j - 1]
+        energy = GPT_energy_grid[j]
 
-        if fine_energy_grid[i] >= prev_energy and fine_energy_grid[i] <= energy:
+        if XGPT_energy_grid[i] >= prev_energy and XGPT_energy_grid[i] <= energy:
             up_binned_vector[i - 1] = down_binned_vector[j - 1]
-            if fine_energy_grid[i] == energy:
+            if XGPT_energy_grid[i] == energy:
                 j += 1
         else:
             SL = down_binned_vector[j - 1]
             SR = down_binned_vector[j]
-            DEL = (energy - fine_energy_grid[i - 1])
-            DER = (fine_energy_grid[i] - energy)
-            up_binned_vector[i-1] = (SL * DEL + SR*DER)/(DEL + DER)
+            DEL = (energy - XGPT_energy_grid[i - 1])
+            DER = (XGPT_energy_grid[i] - energy)
+            up_binned_vector[i-1] = (SL * DEL + SR * DER)/(DEL + DER)
             j += 1
 
     return up_binned_vector
 
 # Projection onto the eigen basis 
-def projection(dict_vector, eigenbasis, labels=perts, energy_grid = fine_energy_grid):
-    projected_vector = np.zeros((len(eigenbasis),))
+def projection(dict_vector, eigenbasis, labels, energy_grid = XGPT_energy_grid):
+    projected_vector = []
+    delta_E = np.diff(energy_grid)
     for label, index in labels.items():
         if index == 0:
             continue
         _, label, _ = label.split("_")
         vector = dict_vector[label]
-        projected_vector[index-1] = trapz(np.array(vector)*np.array(eigenbasis[index-1]), energy_grid[:-1])
-    return projected_vector
 
-def get_GPT_on_eigenbasis(GA_grid):
-    GPT_GA = down_binning(GPT_vector, GA_grid, coarse_energy_grid)
-    GA_energy_grid = energy_from_energy_grid(coarse_energy_grid, GA_grid)
-    extended_GPT_GA = up_binning(fine_energy_grid, GA_energy_grid, GPT_GA)
-    projected_GPT = projection(extended_GPT_GA, eigenbasis)
+        projected_vector.append(np.sum(np.array(vector)*np.array(eigenbasis[index-1])))
+        
+    return np.array(projected_vector)
+
+def project_GPT_onto_eigenbasis(GA_grid):
+    GPT_GA = down_binning(GPT_vector, GA_grid, GPT_energy_grid)
+    GA_energy_grid = energy_from_energy_grid(GPT_energy_grid, GA_grid)
+    extended_GPT_GA = up_binning(XGPT_energy_grid, GA_energy_grid, GPT_GA)
+    projected_GPT = projection(extended_GPT_GA, eigenbasis, perts)
     return projected_GPT
 
 
@@ -104,3 +104,4 @@ def cosine_similarity(vector1, vector2):
     norm_vector2 = np.linalg.norm(vector2)
     similarity = dot_product / (norm_vector1 * norm_vector2)
     return similarity
+
