f12cd9ec09b4be8891568032aa1baf132f337dc1
Emilie3008 <99251374+Emilie3008@users.noreply.github.com>
2024-05-06 09:01:47 +0200

    Update main.py

--- Energy_Grid_Design/main.py
+++ Energy_Grid_Design/main.py
@@ -1,28 +1,25 @@
-
-import matplotlib.pyplot as plt
 from genetic_algorithm import GeneticAlgorithm
-from plot_results import plot_vectors_GPT, plot_vectors_XGPT, compare_integrals
-from data_extraction import coarse_energy_grid, ISOTOPE
-from fitness_utils import energy_from_energy_grid
-import numpy as np
+from plot_results import plot_vectors_GPT, plot_singular_values, plot_eigenfunctions, energy_cut_from_energy_grid, plot_cumulative_cosine_GPT
+from data_extraction import ECCO33, GPT_energy_grid, main_path, ISOTOPE
+import os
 
-for nGroups in range(7,8):
- 
-    genetic_algo = GeneticAlgorithm(nGroups)
+discretisations = {}
+for ff in ["XGPT"]:
+    for seed in range(80, 100):
+        for nGroups in [7, 8, 10, 12, 33]:
+            genetic_algo = GeneticAlgorithm(nGroups, criteria=ff)
+            gen, fitn, energy_cut = genetic_algo.run_genetic_algorithm(seed = seed,
+                                                                    max_iter = 100)
+            with open(os.path.join(main_path,"Tests", ff, "{}_{}G_Pu_allMT.txt".format(ff, nGroups)), "+a") as file:
+                file.write("{}\n".format(energy_cut))
+            
+#     discretisations[nGroups] = energy_cut
+#     plot_cumulative_cosine_GPT(energy_cut)
 
+# plot_vectors_GPT(discretisations, method = ff)
 
-    gen, fitn, energy_cut = genetic_algo.run_genetic_algorithm(seed = 0, max_iter=100) 
+# plot_singular_values()
+# plot_eigenfunctions(tol = 0)
 
-    compare_integrals(energy_cut)
-    plot_vectors_XGPT(energy_cut)
-    plot_vectors_GPT(energy_cut)
-
-    MY_ENERGY_GRID = energy_from_energy_grid(coarse_energy_grid, energy_cut)
-
-    plt.xscale("log")
-    plt.scatter(MY_ENERGY_GRID, np.zeros(nGroups + 1), c = "red")
-    plt.xlabel("Energy (MeV)")
-    plt.title("({}) Energy grid ({} groups) found by comparison of the evaluated GPT "
-              "and xGPT vectors (Pu239, MT = 2, 18, 102)".format(ISOTOPE, nGroups))
-
-    plt.show()
+# energy_cut_ecco = energy_cut_from_energy_grid(GPT_energy_grid, ECCO33)
+# plot_vectors_GPT({33:energy_cut_ecco}, method="ECCO33")
