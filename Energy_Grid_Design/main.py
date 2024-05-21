f3bad59eebe016fdabe8e9154994c28453f47e7c
Emilie3008 <99251374+Emilie3008@users.noreply.github.com>
2024-05-21 11:18:18 +0200

    Update main.py

--- Energy_Grid_Design/main.py
+++ Energy_Grid_Design/main.py
@@ -1,25 +1,19 @@
 from genetic_algorithm import GeneticAlgorithm
-from plot_results import plot_vectors_GPT, plot_singular_values, plot_eigenfunctions, energy_cut_from_energy_grid, plot_cumulative_cosine_GPT
-from data_extraction import ECCO33, GPT_energy_grid, main_path, ISOTOPE
 import os
+from extract_input_data import ISOTOPE
 
-discretisations = {}
-for ff in ["XGPT"]:
-    for seed in range(80, 100):
-        for nGroups in [7, 8, 10, 12, 33]:
+for ff in ["uncertainty", "GPT", "XGPT"]:
+    for seed in range(122, 126):
+        for nGroups in [33]:
             genetic_algo = GeneticAlgorithm(nGroups, criteria=ff)
             gen, fitn, energy_cut = genetic_algo.run_genetic_algorithm(seed = seed,
-                                                                    max_iter = 100)
-            with open(os.path.join(main_path,"Tests", ff, "{}_{}G_Pu_allMT.txt".format(ff, nGroups)), "+a") as file:
+                                                                    max_stagnating_iter=150,
+                                                                    max_iter = 1000)
+            with open(os.path.join(os.getcwd(), "Results",ISOTOPE,
+                             "{}_{}G.txt".format(ff, nGroups)), "+a") as file:
                 file.write("{}\n".format(energy_cut))
-            
-#     discretisations[nGroups] = energy_cut
-#     plot_cumulative_cosine_GPT(energy_cut)
 
-# plot_vectors_GPT(discretisations, method = ff)
 
-# plot_singular_values()
-# plot_eigenfunctions(tol = 0)
+# dis = read_results([10, 12, 22])
 
-# energy_cut_ecco = energy_cut_from_energy_grid(GPT_energy_grid, ECCO33)
-# plot_vectors_GPT({33:energy_cut_ecco}, method="ECCO33")
+# plot_grids(dis)
