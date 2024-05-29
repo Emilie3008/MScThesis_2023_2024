ffb8ae70696fd9a5e2f7111d47d18056d5253129
Emilie3008 <99251374+Emilie3008@users.noreply.github.com>
2024-05-29 09:15:50 +0200

    add representativity comparison function

--- Energy_Grid_Design/plot_results.py
+++ Energy_Grid_Design/plot_results.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-from extract_input_data import gpt_energy_grid, xgpt_energy_grid, ISOTOPE, eigenbasis, perts, gpt_vector_lethargy_normalised, xgpt_vector
-from fitness_utils import down_binning, up_binning, energy_from_energy_grid, evaluate_on_ga, extend, project_gpt_onto_eigenbasis, projection, gpt_vector, xgpt_vector, singular_matrix, cosine_similarity
+from extract_input_data import ECCO33,gpt_energy_grid, xgpt_energy_grid, ISOTOPE, eigenbasis, perts, gpt_vector_lethargy_normalised, xgpt_vector, singular_matrix, gpt_vector, extract_myrrha_sensitivities
+from fitness_utils import down_binning, up_binning, energy_from_energy_grid, evaluate_on_ga, extend, project_gpt_onto_eigenbasis, projection, cosine_similarity
 import numpy as np
 import os
 from extract_results import read_results
@@ -226,15 +226,17 @@ def plot_cosine_contribution_gpt(ga_grid):
     energy = energy_from_energy_grid(gpt_energy_grid, ga_grid)
     naming = "Pu9" if ISOTOPE == "Pu239" else "U8"
     plt.scatter(energy, np.zeros(len(energy)), zorder=30, s=10, c="r", label=f"G-{naming} {len(ga_grid) + 1}")
-    plt.legend()
-    plt.xlim(left=1e-5)
+    plt.legend(fontsize = 7)
+    plt.xlim(left=1e-7)
     plt.show()
 
 
 def plot_grids(grids):
     """
-    :param n_groups: A list of integers, for which we aim to plot the discretisations
-    Plot the sensitivity profile evaluated on the energy grids of the three different fitness functions
+    :param n_groups: A list of integers, for which we aim
+    to plot the discretisations
+    Plot the sensitivity profile evaluated on the energy
+    grids of the three different fitness functions
     """
     for n_group, grid in grids.items():
         fig, axs = plt.subplots(3, 3, figsize=(12, 12), sharex=True, sharey="row")
@@ -259,21 +261,25 @@ def plot_grids(grids):
                 if j == 0:
                     ax.set_ylabel("Sensitivity per unit lethargy", fontsize=7)
 
-                ax.legend(fontsize=8)
+                ax.legend(fontsize=7)
+                ax.set_xlim(1e-6)
+                ax.set_xlabel("E (MeV)", fontsize=6)
 
         fig.add_subplot(111, frame_on=False)
         plt.tick_params(labelcolor="none", top=False, bottom=False, left=False, right=False)
-        plt.xlabel("E (MeV)", fontsize=7)
-        plt.xlim(1e-5)
+        # plt.xlabel("E (MeV)", fontsize=7)
+       
         fig.tight_layout()
         plt.show()
 
 
 def plot_grids_on_other_nuclide(n_groups):
     """
-    :param n_groups: A list of integers, for which we aim to plot the discretisations
-    Plot the sensitivity profile evaluated on the energy grids optimised for 
-    the other nuclide, found with the tree different fitness functions
+    :param n_groups: A list of integers, for which we aim 
+    to plot the discretisations
+    Plot the sensitivity profile evaluated on the energy grids
+    optimised for the other nuclide, found with the three 
+    different fitness functions
     """
     grids = read_results(n_groups, switch=True)
     for n_group, grid in grids.items():
@@ -302,11 +308,149 @@ def plot_grids_on_other_nuclide(n_groups):
                     ax.set_ylabel("Sensitivity per unit lethargy", fontsize=7)
 
                 ax.legend(fontsize=6)
+                                # ax.legend(fontsize=7)
+                ax.set_xlim(1e-6)
+                ax.set_xlabel("E (MeV)", fontsize=6)
 
         fig.add_subplot(111, frame_on=False)
+        nuclide = "$Pu^{239}$" if ISOTOPE=="Pu239" else "$U^{238}$"
+        other = "$Pu^{239}$" if ISOTOPE=="U238" else "$U^{238}$"
+        plt.suptitle("Grids optimised on {} applied to {} sensitivity profiles".format(other,nuclide))
         plt.tick_params(labelcolor="none", top=False, bottom=False, left=False, right=False)
         plt.xlabel("E (MeV)", fontsize=7)
         plt.xlim(1e-5)
         fig.tight_layout()
         plt.show()
 
+
+
+def plot_variability_of_cuts(nGroups):
+    """
+    :param nGroups: the number of groups in the energy 
+    discretisation you wish to visualise the variability
+    of the cuts
+
+    Plot the variability of the cuts for energy grids 
+    containing nGroups groups
+    """
+    for ff in ["GPT", "XGPT", "uncertainty"]:
+        disc = dict((cut,0) for cut in range(1, 226))
+        fig, ax1 = plt.subplots(figsize=(8, 6))
+        i = 0
+        with open(os.path.join(os.getcwd(), "Results",ISOTOPE,
+                                    "{}_{}G.txt".format(ff, nGroups)), "r") as file:
+            for discretisation in file.readlines():
+                i += 1
+                cuts = eval(discretisation.strip())
+                for cut in cuts:
+                    disc[cut] +=1
+
+        histograms = np.array([100*key/i for key in disc.values()])
+        # plt.xscale("log")
+        print(i)
+        ax1.step(gpt_energy_grid[1:-1], histograms, c="red",alpha = 0.6)
+        ax1.set_ylim(ymin = 0, ymax = 600)
+        ax1.set_xscale("log")
+        ax1.set_ylabel("Rate of apparition of the cut (%)")
+        # plt.title(ff)
+        ax2 = ax1.twinx()
+        ax1.set_xlabel("E (MeV)")
+        ax2.set_xscale("log")
+        plt.xlabel("E (MeV)")
+        ax2.set_ylabel("Sensitivity per Unit Lethargy")
+        ax2.step(gpt_energy_grid[:-1], gpt_vector_lethargy_normalised["MT18"], c="royalblue", label="MT18 sensitivity profile", alpha = 0.4)
+        ax2.step(gpt_energy_grid[:-1], gpt_vector_lethargy_normalised["MT102"], c="darkgreen", linestyle = "-",label="MT102 sensitivity profile", alpha = 0.4)
+        n = "U8" if ISOTOPE == "U238" else "Pu9"
+        plt.title("Variability of the cuts for {}-{} {} (50 runs)".format("X" if ff=="XGPT" else "G" if ff== "GPT" else "V",n, nGroups, i))
+        plt.legend()
+        plt.xlim(1e-6)
+        plt.show()
+
+def plot_ecco33():
+    """
+    Plot the sensitivity profiles evaluated on the ECCO 33
+    """
+
+    fig, axs = plt.subplots(3, 1, figsize=(4, 6))  # 3 lignes, 1 colonne
+    ecco33 = energy_cut_from_energy_grid(gpt_energy_grid, ECCO33)
+    down_binned = down_binning(gpt_vector_lethargy_normalised, ecco33, gpt_energy_grid)
+    ga_grid = energy_from_energy_grid(gpt_energy_grid, ecco33)
+ 
+    axs[0].step(gpt_energy_grid[:-1], gpt_vector_lethargy_normalised["MT2"], c="grey", alpha=0.5)
+    axs[0].set_xscale("log")
+    axs[0].step(gpt_energy_grid[:-1], extend(gpt_energy_grid, ga_grid, down_binned["MT2"]), 'b-', label = "ECCO 33")
+    axs[0].set_title('($Pu^{239}$) Sensitivity profile of $k_{eff}$, under MT2 cross-section perturbation', fontsize=9)
+    axs[0].set_xlabel('E (MeV)', fontsize=7)
+    axs[0].set_xlim(1e-5)
+    axs[0].set_ylabel('Sensitivity per unit lethargy', fontsize=8)
+    axs[0].legend()
+    
+    axs[1].set_xscale("log")
+    axs[1].step(gpt_energy_grid[:-1], gpt_vector_lethargy_normalised["MT18"], c="grey", alpha=0.5)
+    axs[1].set_xlim(1e-5)
+    axs[1].step(gpt_energy_grid[:-1], extend(gpt_energy_grid, ga_grid, down_binned["MT18"]), 'b-',  label = "ECCO 33")
+    axs[1].set_title('($Pu^{239}$) Sensitivity profile of $k_{eff}$, under MT18 cross-section perturbation', fontsize=9)
+    axs[1].set_xlabel('E (MeV)', fontsize=7)
+    axs[1].set_ylabel('Sensitivity per unit lethargy', fontsize=8)
+    axs[1].legend()
+   
+    axs[2].set_xscale("log")
+    axs[2].step(gpt_energy_grid[:-1], gpt_vector_lethargy_normalised["MT102"], c="grey", alpha=0.5)
+    axs[2].step(gpt_energy_grid[:-1], extend(gpt_energy_grid, ga_grid, down_binned["MT102"]), 'b-',  label = "ECCO 33")
+    axs[2].set_title('($Pu^{239}$) Sensitivity profile of $k_{eff}$, under MT102 cross-section perturbation', fontsize=9)
+    axs[2].set_xlabel('E (MeV)', fontsize=7)
+    axs[2].set_ylabel('Sensitivity per unit lethargy', fontsize=8)
+    axs[2].set_xlim(1e-5)
+    axs[2].legend()
+
+    plt.tight_layout()
+    
+    plt.show()
+
+def compute_representativity(list_grids):
+    """
+    :param list_grids: a list of the names of energy grids
+    we aim at comparing the representativity of ALFRED core
+    to MYRRHA core. This list should 
+    be filled with strings of the following format :
+            - The first letter is V, X, or G
+            - U if the grid is optimised on Uranium 238 and
+              Pu if the grid is optimised on Plutonium 239
+            - The number of groups in the energy discretisation
+        As an example, if you want to compute the representativity
+        for sensitivities scored on an energy grid optimised with
+        the XGPT cosine similarity comparison of the sensitivity
+        profiles of Pu239 and containing 33 groups, add "XPu33"
+        in the list.
+
+    Print the results of the evaluated representativity and
+    their discrepancy with the target repreresentativity (226G)
+    """
+        
+    dict_repres = {}
+    myrrha_sens, myrrha_per_unit_leth= extract_myrrha_sensitivities()
+    r = compute_representativity(myrrha_sens, gpt_vector, "{}_xs_reduced.csv".format(ISOTOPE))
+   
+    ecco33 = energy_cut_from_energy_grid(gpt_energy_grid, ECCO33)
+
+    myrrha_rebinned = down_binning(myrrha_sens,ecco33, gpt_energy_grid)
+    alfred_rebinned = down_binning(gpt_vector, ecco33, gpt_energy_grid)
+    r_eval = compute_representativity(myrrha_rebinned, alfred_rebinned, "ECCO33_{}_xs_reduced.csv".format(ISOTOPE))
+
+    dict_repres["ECCO33"]= (r_eval, abs(r-r_eval)/r)
+
+    dis = read_results([ 33])
+   
+    for energy_grid_name in list_grids:
+    
+        method = "uncertainty" if energy_grid_name[0]=="V" else "GPT" if energy_grid_name[0]=="G" else "XGPT"
+
+        alfred_rebinned = down_binning(gpt_vector, dis[int(energy_grid_name[-2:])][method][1], gpt_energy_grid)
+        myrrha_rebinned = down_binning(myrrha_sens, dis[int(energy_grid_name[-2:])][method][1], gpt_energy_grid)
+        r_eval = compute_representativity(myrrha_rebinned, alfred_rebinned, "{}_{}_xs_reduced.csv".format(energy_grid_name, ISOTOPE))
+
+        dict_repres[energy_grid_name]=(r_eval,abs(r-r_eval)/r)
+
+    sorted_dictionnary = dict(sorted(dict_repres.items(), key=lambda item: item[1][0]))
+
+    print(sorted_dictionnary)
